6846173420c5bb4031c255eaf6facf3e3f942431
Ellis <mortinellis@gmail.com>
2020-03-13 15:51:35 +0000

    added inverse index explanation

---
+++ local-search-engine.py
@@ -0,0 +1,15 @@
+###############################################################################################
+##
+## local search engine utilising inverted index and query engine
+##
+## the index saves files in alphabetical order. the alphabetical order is also numbered:
+## for example, if the first word is bird and the second word is car, it will print as so;
+## 1, bird
+## 2, car
+## however, the index also shows the document that the elements came from. so, after the word,
+## it will print the name of the document.
+##
+###############################################################################################
+
+def inversed_index(i_file):
+	
